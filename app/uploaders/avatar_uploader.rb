dbbf21513a268046256362e67a16baee2cb3603f
平原颯太 <>
2025-11-12 18:16:13 +0900

    アバター画像をクリックして変更。それをすぐに反映

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,6 +10,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [ 512, 512 ]
 
   def extensions_allowlist
-    %w[jpg jpeg gif png]
+    %w[jpg jpeg gif png svg svgz]
+  end
+
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path('avatars/default_avatar.svg')
   end
 end
