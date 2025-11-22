655ba3d72c4d2f0c7c727f1f7836d41043cd8e4f
平原颯太 <>
2025-11-22 16:04:43 +0900

    アプリホーム画面の作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,15 @@
 class TopController < ApplicationController
   def index
+    @mokumoku_sessions = MokumokuSession.valid
+                                        .includes(:user)
+                                        .where(users: { is_deleted: false })
+                                        .order(created_at: :desc)
+                                        .limit(3)
+
+    @material_reviews = MaterialReview.valid
+                                      .includes(:user)
+                                      .where(users: { is_deleted: false })
+                                      .order(created_at: :desc)
+                                      .limit(3)
   end
 end
