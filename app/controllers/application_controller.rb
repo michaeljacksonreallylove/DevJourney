0485d95da2fe2817e49e88df6b7d64558b2dea34
平原颯太 <>
2025-11-23 10:18:03 +0900

    DEV-16 セッションが切れたときのフラッシュにbooleanのフラッシュも入るので非表示にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :remove_boolean_flash, if: -> { flash.any? }
 
   protected
 
@@ -17,5 +18,13 @@ class ApplicationController < ActionController::Base
 
     def after_sign_out_path_for(resource_or_scope)
       new_user_session_path
+    end
+
+  private
+    # フラッシュに入るbooleanの表示を削除
+    def remove_boolean_flash
+      flash.each do |key, value|
+        flash.delete(key) if value == true || value == false
+      end
     end
 end
