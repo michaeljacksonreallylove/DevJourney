5d6949da74deafc1ccc20d401c15f0236bd29053
平原颯太 <>
2025-11-30 15:54:47 +0900

    DEV-8 params.require.permitをRails8に合わせた書き方に変更

--- app/controllers/material_reviews_controller.rb
+++ app/controllers/material_reviews_controller.rb
@@ -28,6 +28,6 @@ class MaterialReviewsController < ApplicationController
 
   private
     def material_review_params
-      params.require(:material_review).permit(:user_id, :title, :material_url, :score, :description)
+      params.expect(material_review: [:user_id, :title, :material_url, :score, :description])
     end
 end
