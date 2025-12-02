4dbc38643d4e0fe1d6c1f81fa019b53e743cde72
平原颯太 <>
2025-12-02 18:51:39 +0900

    DEV-19 importmapからjsbundling-railsに移行する(React導入への準備)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "bootstrap"
-import "@popperjs/core"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
-import MyHello from "./components/my-hello"
-
-const root = ReactDOM.createRoot(document.querySelector("#app"));
-root.render(<MyHello name="あああ" />)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
--- app/javascript/components/my-hello.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function MyHello({ name }) {
-  return <div>Hello, {name}!</div>
-}
