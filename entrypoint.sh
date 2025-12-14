4b560bb10c5f4b8cd4bf9f9212358ab1ed438e4f
平原颯太 <>
2025-12-14 13:53:11 +0900

    DEV-20 Dockerで環境構築を行うための設定を行う

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -f tmp/pids/server.pid ]; then
-  rm tmp/pids/server.pid
-fi
-
-echo "Waiting for database..."
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q' 2>/dev/null; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
-echo "PostgreSQL is up - executing command"
-
-echo "Running db:prepare..."
 bundle exec rails db:prepare
 
 exec "$@"
